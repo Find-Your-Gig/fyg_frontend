a7b51169386ec34ab3234e867ea01aee8a16ecfa
Pulkit Kalra <pulkit.kalra5678@gmail.com>
2024-09-06 00:41:11 -0400

    added notification feature

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,26 +1,53 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { AiOutlineSearch, AiOutlineShoppingCart, AiOutlineUser, AiOutlineShop } from 'react-icons/ai';
-import { dropdownMenu } from '../../data/headerData';
+import { AiOutlineSearch, AiOutlineBell, AiOutlineUser, AiOutlineShop } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
+import { BASEURL } from '../../util/Util';
 import commonContext from '../../contexts/common/commonContext';
 import cartContext from '../../contexts/cart/cartContext';
 import LoginForm from '../form/login/LoginForm';
 import SearchBar from './SearchBar';
-
+import axios from 'axios';
+import './Common.scss';
 
 const Header = () => {
-
     const { formUserInfo, toggleForm, toggleSearch, isLoggedIn, setLoading, loginResponse, setLoggedIn, setFormUserInfo, setLoginResponse } = useContext(commonContext);
     const { cartItems } = useContext(cartContext);
     const [isSticky, setIsSticky] = useState(false);
+    const [notifications, setNotifications] = useState([]);
+    const [showDropdown, setShowDropdown] = useState(false);
     const navigate = useNavigate();
+    const dropdownRef = useRef(null);
 
+    const fetchNotifications = async () => {
+        try {
+            const response = await axios.post(`${BASEURL}/notification`, {
+                userId: loginResponse.userId,
+                userType: loginResponse.type
+            });
+            setNotifications(response.data.notifications);
+        } catch (error) {
+            console.error('Error fetching notifications:', error);
+        }
+    };
+
+    // Close dropdown on clicking outside
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setShowDropdown(false);
+            }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [dropdownRef]);
 
     // handle the sticky-header
     useEffect(() => {
         const handleIsSticky = () => window.scrollY >= 50 ? setIsSticky(true) : setIsSticky(false);
-
         window.addEventListener('scroll', handleIsSticky);
 
         return () => {
@@ -28,18 +55,53 @@ const Header = () => {
         };
     }, [isSticky]);
 
+    // Fetch notifications from the backend
+    useEffect(() => {
+        if (isLoggedIn) {
+            fetchNotifications();
+        }
+    }, [loginResponse, isLoggedIn]);
 
-    const cartQuantity = cartItems.length;
+    useEffect(() => {
+
+    }, [notifications])
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
 
     const handleAllProducts = () => {
         setLoading(true);
-    }
-    
+    };
+
     const handleSignOut = () => {
         setLoggedIn(false);
         setFormUserInfo('');
         setLoginResponse({});
         navigate('/');
+    };
+
+    const updateNotification = async (id) => {
+        try {
+            const response = await axios.post(`${BASEURL}/notification/update`, {
+                userId: loginResponse.userId,
+                userType: loginResponse.type,
+                id: id
+            });
+            setNotifications(response.data.notifications);
+        } catch (error) {
+            console.error('Error fetching notifications:', error);
+        }
+    }
+    
+    const handleNotificationClick = (id) => {
+        updateNotification(id);
+        toggleDropdown();
+        if (loginResponse.type === 'COMPANY') {
+            navigate('/reservations?userId='+loginResponse.userId);
+        } else if (loginResponse.type === 'ARTIST') {
+            navigate('/invitations?userId='+loginResponse.userId);
+        }
     }
 
     return (
@@ -65,17 +127,30 @@ const Header = () => {
                                 <div className="tooltip">Artists</div>
                             </div>
 
-                            {/* <div className="cart_action">
-                                <Link to="/cart">
-                                    <AiOutlineShoppingCart />
-                                    {
-                                        cartQuantity > 0 && (
-                                            <span className="badge">{cartQuantity}</span>
-                                        )
-                                    }
-                                </Link>
-                                <div className="tooltip">Cart</div>
-                            </div> */}
+                            {/* Bell Icon for Notifications */}
+                            <div className="notifications_action" ref={dropdownRef}>
+                                <span className="bell_icon" onClick={toggleDropdown}>
+                                    <AiOutlineBell />
+                                    {notifications.length > 0 && (
+                                        <span className="badge">{notifications.length}</span>
+                                    )}
+                                </span>
+
+                                {isLoggedIn && showDropdown && (
+                                    <div className="dropdown_menu notifications_dropdown">
+                                        <h4>Notifications</h4>
+                                        <ul>
+                                            {notifications.length === 0 ? (
+                                                <li>No notifications</li>
+                                            ) : (
+                                                notifications.map(notification => (
+                                                    <li key={notification.id} onClick={() => handleNotificationClick(notification.id)}>{notification.text}</li>
+                                                ))
+                                            )}
+                                        </ul>
+                                    </div>
+                                )}
+                            </div>
 
                             <div className="user_action">
                                 <span>
